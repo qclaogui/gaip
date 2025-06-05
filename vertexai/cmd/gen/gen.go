ede866ed0fc2dbd3f335f64433fed21e6f957407
Weifeng Wang <qclaogui@gmail.com>
2025-06-05 13:18:03 +0800

    Signed-off-by: Weifeng Wang <qclaogui@gmail.com>

--- vertexai/cmd/gen/gen.go
+++ vertexai/cmd/gen/gen.go
@@ -2,20 +2,6 @@
 //
 // Licensed under the Apache License 2.0.
 
-// Copyright 2023 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package main
 
 import (
@@ -36,7 +22,6 @@ import (
 var (
 	project   = flag.String("project", "", "project ID")
 	location  = flag.String("location", "", "location")
-	temp      = flag.Float64("t", 0.2, "temperature")
 	model     = flag.String("model", "", "model")
 	streaming = flag.Bool("stream", false, "using the streaming API")
 )
@@ -53,9 +38,8 @@ func main() {
 		log.Fatal(err)
 	}
 	defer client.Close()
-	model := client.GenerativeModel(*model)
-	model.Temperature = float32(*temp)
 
+	model := client.GenerativeModel(*model)
 	model.SafetySettings = []*genai.SafetySetting{
 		{
 			Category:  genai.HarmCategorySexuallyExplicit,
